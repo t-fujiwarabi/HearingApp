fa8ba7e2cb7fd49f04897dc2817b79347304a6e9
t-fujiwarabi <t.fujiwarabi@gmail.com>
2025-10-22 10:27:57 +0900

    Initial Commit

---
+++ HearingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HearingApp
+//
+//  Created by 藤原崇志 on 2025/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HearingAppTests/HearingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HearingAppTests.swift
+//  HearingAppTests
+//
+//  Created by 藤原崇志 on 2025/10/22.
+//
+
+import Testing
+@testable import HearingApp
+
+struct HearingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
